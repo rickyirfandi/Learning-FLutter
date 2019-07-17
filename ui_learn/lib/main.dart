41cdf31f1e1dcdeafb9cce5ef0002e6882fb1a60
Ricky Irfandi <ricky.irfandi2@gmail.com>
2019-07-18 00:11:29 +0700

    Nambah inkwel buat listener di card,
    jadi bisa nampilin bottom sheet pas diteken lama,

--- ui_learn/lib/main.dart
+++ ui_learn/lib/main.dart
@@ -102,45 +102,49 @@ class HomePage extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10))),
                 elevation: 2,
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      height: 150,
-                      width: 0.4 * MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("images/isyan.jpg"),
-                            fit: BoxFit.fitHeight),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(10),
-                          bottomLeft: Radius.circular(10),
+                child: InkWell(
+                  onLongPress: (){_settingModalBottomSheet(context);},
+                  splashColor: Colors.lightBlueAccent[500],
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 150,
+                        width: 0.4 * MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/isyan.jpg"),
+                              fit: BoxFit.fitHeight),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height: 150,
-                      width: 0.4 * MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.all(10),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            "Isyana Sarasvati",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
+                      Container(
+                        height: 150,
+                        width: 0.4 * MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.all(10),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              "Isyana Sarasvati",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                          Container(height:10),
-                          Text(
-                            "Isyana Sarasvati merupakan penyanyi dan penulis lagu berkebangsaan Indonesia. Isyana merupakan lulusan dari Nanyang Academy ",
-                            style: TextStyle(
-                              fontSize: 12,
+                            Container(height:10),
+                            Text(
+                              "Isyana Sarasvati merupakan penyanyi dan penulis lagu berkebangsaan Indonesia. Isyana merupakan lulusan dari Nanyang Academy ",
+                              style: TextStyle(
+                                fontSize: 12,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -304,4 +308,28 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+}
+
+void _settingModalBottomSheet(context){
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc){
+        return Container(
+          child: new Wrap(
+            children: <Widget>[
+              new ListTile(
+                  leading: new Icon(Icons.star),
+                  title: new Text('Favourite'),
+                  onTap: () => {}
+              ),
+              new ListTile(
+                leading: new Icon(Icons.delete),
+                title: new Text('Delete'),
+                onTap: () => {},
+              ),
+            ],
+          ),
+        );
+      }
+  );
 }
