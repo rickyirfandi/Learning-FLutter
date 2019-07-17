09643c9e935584a61aee0fba06502eed504dc25a
Ricky Irfandi <ricky.irfandi2@gmail.com>
2019-07-17 23:55:24 +0700

    day 2 : Bisa bikin list avatar bunder kesamping

--- ui_learn/lib/main.dart
+++ ui_learn/lib/main.dart
@@ -17,19 +17,82 @@ class HomePage extends StatelessWidget {
         appBar: PreferredSize(
           child: AppBar(
             elevation: 5,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("images/isyan.jpg"),
-                  fit: BoxFit.fitHeight,
+            flexibleSpace: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("images/isyan.jpg"),
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  height: 75,
+                  margin: EdgeInsets.only(top: 0.3 * MediaQuery.of(context).size.height - 60 ),
+                  child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                            child: Text('Isyan'),
+                          ),
+                        ),
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                          ),
+                        ),
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                            child: Icon(Icons.access_alarm),
+                          ),
+                        ),
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                          ),
+                        ),
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                          ),
+                        ),
+                        Container(
+                          height: 75,
+                          width: 75,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/isyan.jpg"),
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
+              ],
             ),
           ),
           preferredSize:
               Size.fromHeight(0.3 * MediaQuery.of(context).size.height),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
             Container(
               height: 150,
@@ -77,6 +140,56 @@ class HomePage extends StatelessWidget {
                         ],
                       ),
                     )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              width: 0.9 * MediaQuery.of(context).size.width,
+              margin: EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                elevation: 2,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 150,
+                      width: 0.4 * MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("images/isyan.jpg"),
+                            fit: BoxFit.fitHeight),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      width: 0.4 * MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "Isyana Sarasvati",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Container(height:10),
+                          Text(
+                            "Isyana Sarasvati merupakan penyanyi dan penulis lagu berkebangsaan Indonesia. Isyana merupakan lulusan dari Nanyang Academy ",
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
